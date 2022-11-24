13d53250549e6444ddba6194603dfef9227f0c22
Xhaoo F <fxhaoo@163.com>
2022-11-24 23:10:07 +0800

    修改docker

--- run.py
+++ run.py
@@ -2,6 +2,12 @@ from .wx_sql import Wx_Mysql
 import re
 from decimal import *
 
+from flask import Flask
+from flask import request
+import hashlib
+
+app = Flask(__name__)
+
 
 class Wx_Reply(Wx_Mysql):
 
@@ -30,6 +36,33 @@ class Wx_Reply(Wx_Mysql):
             difference = (price - account_payable)
             shou_user.append(user) if difference > 0 else fu_user.append(user)
             user_info["{}".format(user)] = [price, difference]
+
+
+@app.route('/wechat')
+def wechat():
+    signature = request.args.get("signature", "")
+    timestamp = request.args.get("timestamp", "")
+    nonce = request.args.get("nonce", "")
+    echostr = request.args.get("echostr", "")
+    print(signature, timestamp, nonce, echostr)
+
+    token = "fxhaoo"
+
+    # 2、 进行字典排序
+    data = [token, timestamp, nonce]
+    data.sort()
+
+    # 3、三个参数拼接成一个字符串并进行sha1加密
+    temp = ''.join(data)
+    sha1 = hashlib.sha1(temp.encode('utf-8'))
+    hashcode = sha1.hexdigest()
+    print(hashcode)
+
+    # 4、对比获取到的signature与根据上面token生成的hashcode，如果一致，则返回echostr，对接成功
+    if hashcode == signature:
+        return echostr
+    else:
+        return "error"
 
 
 if __name__ == '__main__':
